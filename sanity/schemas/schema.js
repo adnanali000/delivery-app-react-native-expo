225d124c5a57f37cc6bd734b172742284316621a
adnanali000 <adnan.museel123@gmail.com>
2022-08-24 22:48:30 +0500

    sanity client work done

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -5,17 +5,17 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // We import object and document schemas
-import blockContent from './blockContent'
 import category from './category'
 import restaurant from './restaurant'
-import author from './author'
+import dish from './dish'
+import featured from './featured'
 
 export default createSchema({
   name: 'default',
   types: schemaTypes.concat([
     restaurant,
-    author,
+    dish,
     category,
-    blockContent,
+    featured
   ]),
 })
